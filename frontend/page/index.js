e6e9866a6a17dd5724316bf2d8e7fd9d7d5ed484
Junkazama87 <64848168+kairakazama877@users.noreply.github.com>
2026-02-05 19:48:44 +0800

    Add files via upload

---
+++ frontend/page/index.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Pharos Builder Incubator Program</h1>
+      <p>
+        Discovering visionary teams in RWA, DeFi, and Yield Infrastructure.
+      </p>
+    </main>
+  );
+}
